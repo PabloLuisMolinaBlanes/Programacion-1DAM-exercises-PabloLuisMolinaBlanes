7c6666b98c322014f6709f49ae8cc14d91e6b27f
Pablo Luis Molina Blanes <pablomb255@gmail.com>
2019-09-26 09:01:48 +0200

    Changed code

--- Tema2/MisDatosPersonales.java
+++ Tema2/MisDatosPersonales.java
@@ -1,7 +1,7 @@
 public class MisDatosPersonales {
 	static String miNombre = "Pablo Luis Molina Blanes";
-	static String miTelefono = "666274793";
-	static String miDireccion = "Calle Antonio Machado, 11";
+	static String miTelefono = "miTelefono";
+	static String miDireccion = "miDireccion";
 	public static void main(String[] args) {
 		System.out.println(miNombre);
 		System.out.println(miTelefono);
