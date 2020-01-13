83d5dbcd9a2e41930237bdde8ac0b7470e757333
Pablo Molina <pablomb255@gmail.com>
2020-01-13 08:32:11 +0100

    Fixed error uploading

---
+++ Tema9/PilotoDeFormula1.java
@@ -0,0 +1,10 @@
+package Tema9;
+
+public class PilotoDeFormula1 {
+int carrerasGanadas;
+String paisQueRepresenta;
+int primeranocompitiendo;
+}
+// Ejercicio 5: Jugador, Campo, Equipo, Entrenador, MiembrosConEntradaProhibida
+// Ejercicio 4: Precio, Calle, Numero, Estado.
+// 
